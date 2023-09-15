e3e33407c2096b0829031aee1863df963443668e
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2023-09-14 21:30:04 -0400

    initial login page w/ fonts in globals

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,21 @@
+import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react'
+import chakraTheme from '@chakra-ui/theme'
+import { Input } from '@chakra-ui/react'
+import { Stack } from '@chakra-ui/react'
+import LoginForm from '../components/LoginForm/LoginForm'
+import './LoginPage.scss'
+
+function LoginPage(){
+
+
+    return(
+
+            <LoginForm/>
+
+
+
+    )
+}
+
+
+export default LoginPage;
