540772a41b01ff03d52111975104103d5933013b
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2023-09-14 21:34:36 -0400

    add forgot password to form

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,29 @@
+// src/components/LoginForm.js
+import { Box, Heading, Input, Button, FormControl, FormLabel, Stack } from "@chakra-ui/react";
+import {Link} from 'react-router-dom'
+
+const LoginForm = () => {
+  return (
+    <Box maxW="sm" p="4" borderWidth="1px" borderRadius="lg">
+      <Heading as="h2" size="md" mb="4" fontFamily='Lush Handwritten Compressed'>
+        LUSH
+      </Heading>
+      <form>
+        <Stack spacing="4">
+          <FormControl id="username">
+            <Input type="text" placeholder="username" />
+          </FormControl>
+          <FormControl id="password">
+            <Input type="password" placeholder="password" />
+          </FormControl>
+          <Link to='/'>Forgot Password?</Link>
+          <Button type="submit" bg="rgb(180, 157, 253)" fontFamily='Lush Handwritten Compressed' _hover={{ bg: 'white' }}>
+            SIGN-IN
+          </Button>
+        </Stack>
+      </form>
+    </Box>
+  );
+};
+
+export default LoginForm;
