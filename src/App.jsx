b2413724959199e9c08b62c48e816f023b2ea3a9
Vivian Yu <vivianlamyu@gmail.com>
2023-09-16 17:22:25 -0400

    Added StatusBar component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import StatusBar from './components/StatusBar/StatusBar';
 import LoginPage from './pages/LoginPage/LoginPage';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
 import QueuePage from './pages/QueuePage/QueuePage';
@@ -8,6 +9,7 @@ import './styles/_globals.scss';
 function App() {
   return (
     <BrowserRouter>
+    <StatusBar />
       <Routes>
         <Route path='/' element='' />
         <Route path='/login' element={<LoginPage/>} />
