6681ae95b411e4cbfd6c71dbb15480a54d88746c
Armon Luckenbach <armonluck@gmail.com>
2023-09-18 18:45:03 -0400

    Adding some practice code from Chakra UI and styling to include bg colors and padding from UX design team

--- src/components/AboutProduct.jsx
+++ src/components/AboutProduct.jsx
@@ -16,9 +16,45 @@ import {
 import '../styles/_fonts.scss';
 
 function AboutProduct() {
-  return (
-    <div>AboutProduct</div>
-  )
+    return (
+        <Box px='20px' bg='#282121' paddingBottom='20px'>
+            <Accordion allowToggle color='#FFF'>
+                <AccordionItem>
+                    <h2>
+                        <AccordionButton>
+                            <Box as="span" flex='1' textAlign='left'>
+                                Section 1 title
+                            </Box>
+                            <AccordionIcon />
+                        </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat.
+                    </AccordionPanel>
+                </AccordionItem>
+
+                <AccordionItem>
+                    <h2>
+                        <AccordionButton>
+                            <Box as="span" flex='1' textAlign='left'>
+                                Section 2 title
+                            </Box>
+                            <AccordionIcon />
+                        </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat.
+                    </AccordionPanel>
+                </AccordionItem>
+            </Accordion>
+        </Box>
+    )
 }
 
 export default AboutProduct;
