3bceddaaeefa42ecb95cffe863cb63130ca978e7
Vivian Yu <vivianlamyu@gmail.com>
2023-09-18 18:07:41 -0400

    Added LushLens component and updated the images for BuyAgain and BestSellers component

--- src/components/BestSellers/BestSellers.jsx
+++ src/components/BestSellers/BestSellers.jsx
@@ -1,30 +1,13 @@
-import { Flex, Box, Text, VStack } from '@chakra-ui/react';
-import BestSellersIcon from '../../assets/icons/mdi_fire.svg';
-import LayerOne from '../../assets/icons/Rectangle 17.svg';
-import LayerTwo from '../../assets/icons/Rectangle 18.svg';
+import { Box } from '@chakra-ui/react';
+import BestSellersImage from '../../assets/icons/Best Sellers.png';
 import './BestSellers.scss'
 
 const BestSellers = () => {
-    const flexBoxStyle = {
-        w: '101px',
-        h: '112px',
-        p: '8px 17px 10px 16px',
-        bg: '#E2D7FC',
-        color: '#000', 
-        boxShadow:'-4px 5px 14px 0px rgba(0, 0, 0, 0.15)',
-        borderRadius: '10px',
-        position: 'relative'
-    };
-
     return(
-        <Flex sx={flexBoxStyle}>
-            <VStack spacing={0} gap='10px' m='auto'>
-                <Text className='bestSellers'>Best Sellers</Text>
-                <img className='layerOne' src={LayerOne} alt='Best Sellers'/>
-                <img className='layerTwo' src={LayerTwo} alt='Best Sellers'/>
-                <img className='bestSellersIcon' src={BestSellersIcon} alt='Best Sellers'/>
-            </VStack>
-        </Flex>
+        <Box className='bestSellersContainer'>
+            <img className='bestSellersImage' src={BestSellersImage} alt='Best Sellers'></img>
+
+        </Box>
     );
 };
 
--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,17 +1,19 @@
 
 import React from 'react';
-import { Flex, Box } from '@chakra-ui/react';
+import { Flex, Box, VStack } from '@chakra-ui/react';
 import BestSellers from '../../components/BestSellers/BestSellers';
 import BuyAgain from '../../components/BuyAgain/BuyAgain';
-import OnSale from '../../components/OnSale/OnSale';
+import LushLens from '../../components/LushLens/LushLens';
 
 const ProfilePage = () => {
     return (
         <Box height='100vh' background= 'var(--neutral-900, #282121)' pt='40px'>
-            <Flex justifyContent='center' gap='24px' px='20px'>
-                <BestSellers />
-                <BuyAgain />
-                <OnSale />
+            <Flex justifyContent='center'>
+                <VStack spacing='12px' px='20px'>
+                    <LushLens />
+                    <BuyAgain />
+                    <BestSellers />
+                </VStack>
             </Flex>
         </Box>
     )
