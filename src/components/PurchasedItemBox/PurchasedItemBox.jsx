81fe76a435be2fab5be15b529c8ec1237e3bca6c
Vivian Yu <vivianlamyu@gmail.com>
2023-09-18 11:40:39 -0400

    Updated the 3 bath bomb pictures within the PurchaseItemBox component

--- src/components/PurchasedItemBox/PurchasedItemBox.jsx
+++ src/components/PurchasedItemBox/PurchasedItemBox.jsx
@@ -1,5 +1,7 @@
-import { Box, VStack, Text, Flex } from '@chakra-ui/react';
-import BathBombIcon from '../../assets/icons/5b406c976228ca987cb13b9d272cec65.png';
+import { Box, VStack, HStack, Text, Flex } from '@chakra-ui/react';
+import BathBombOne from '../../assets/icons/5b406c976228ca987cb13b9d272cec65.png';
+import BathBombTwo from '../../assets/icons/Bathbomb_2-transformed.png';
+import BathBombThree from '../../assets/icons/Bathbomb_4-transformed.png';
 import './PurchasedItemBox.scss'
 
 const PurchasedItemBox = () => {
@@ -9,20 +11,28 @@ const PurchasedItemBox = () => {
         borderRadius: '10px',
         bg: 'var(--Item-Background, #F3F2F3)',
         boxShadow: '0px 5px 10px 0px rgba(0, 0, 0, 0.25)',
+        // padding: '17px 0 11px 28px',
         position: 'relative',
+        alignItems: 'flex-start'
     };
 
     return (
         <Flex sx={flexBoxStyle}>
-            {/* To be replaced with back-end data */}
-            <img className='bathBombIcon'src={BathBombIcon} alt='Bath Bomb Item' />
-            <VStack spacing={0}>
-                <Box pt='16px' justifyContent='flex-start'>
-                    <Text className='purchase-date'>Purchase date</Text>
-                    <Text className='purchase-date'>Sep 14, 2023</Text>
-                    <Text className='purchase-amount'>$18.00</Text>
+            <HStack spacing={0}>
+                {/* To be replaced with back-end data */}
+                <Box position='relative'>
+                    <img className='bathBombOne'src={BathBombOne} alt='Bath Bomb Item' />
+                    <img className='bathBombTwo'src={BathBombTwo} alt='Bath Bomb Item' />
+                    <img className='bathBombThree'src={BathBombThree} alt='Bath Bomb Item' />
                 </Box>
-            </VStack>
+                <VStack spacing={0}>
+                    <Box pl='30px' justifyContent='flex-start'>
+                        <Text className='purchase-date'>Purchase date</Text>
+                        <Text className='purchase-date'>Sep 14, 2023</Text>
+                        <Text className='purchase-amount'>$37.50</Text>
+                    </Box>
+                </VStack>
+            </HStack>
             <Text className='purchase-receipt'>View Receipt</Text>
         </Flex>
     );
