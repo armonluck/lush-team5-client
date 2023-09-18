088a5ba7a28221e8f3fb8548b50f17d3ae939aca
Armon Luckenbach <armonluck@gmail.com>
2023-09-18 18:41:03 -0400

    Adding component to Route on App.jsx for easier viewing and changed internal path to access Lush Handwritten font file

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import StatusBar from './components/StatusBar/StatusBar';
 import LoginPage from './pages/LoginPage/LoginPage';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
 import QueuePage from './pages/QueuePage/QueuePage';
+import AboutProduct from './components/AboutProduct';
 import './styles/_globals.scss';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path='/login' element={<LoginPage/>} />
         <Route path='/profile' element={<ProfilePage />} />
         <Route path='/queue' element={<QueuePage />} />
+        <Route path='/about-product' element={<AboutProduct />} />
       </Routes>
     </BrowserRouter>
   );
