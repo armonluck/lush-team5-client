6177e6e071bcb1acf03c30da62a512e5d2a06542
Vivian Yu <vivianlamyu@gmail.com>
2023-09-18 11:57:00 -0400

    Updated the queue page with Hi-Fi edit

--- src/pages/QueuePage/QueuePage.jsx
+++ src/pages/QueuePage/QueuePage.jsx
@@ -2,34 +2,33 @@
 import React from 'react';
 import { Flex, Text, Box, VStack } from '@chakra-ui/react';
 import PurchasedItemBox from '../../components/PurchasedItemBox/PurchasedItemBox';
+import ClockIcon from '../../assets/icons/ph_clock.svg';
 import './QueuePage.scss'
 
 const QueuePage = () => {
     return (
         <Flex height='100vh' background= 'var(--neutral-900, #282121)' color='#FFF' p='24px 21px 0 21px'>
             <VStack spacing={0}>
-                <Text fontFamily='Lush Handwritten' fontSize='30px' pb='16px'>
-                Purchase Completed
+                <Text fontFamily='Lush Handwritten' color='#32BF00' fontSize='30px' pb='16px'>
+                    Purchase Completed
                 </Text>
                 <PurchasedItemBox />
                 <Box w='348px' h='auto' pt='24px'>
                     <VStack spacing={0} alignItems='flex-start' gap='20px'>
                         <Text className='ready-message'>
-                            Thanks for shopping at Lush! 
+                            <span className='ready-message__name'>Natasha</span>, Thanks for shopping at Lush! 
                         </Text>
                         <Text className='ready-message'>
-                            {/* Replace Franchesca with back-end user */}
-                            <span className='ready-message__name'>Franchesca</span> is packing your order, so keep looking around, and we’ll come bring your order to you when it’s ready!
-                        </Text>
+                            Keep shopping around, and we’ll bring your order when it’s ready!</Text>
                     </VStack>
                 </Box>
-                <Text fontFamily='Lush Handwritten' fontSize='40px' pt='43px'>
-                    Est. Wait Time
+                <Text fontFamily='Helvetica' fontSize='30px' pt='43px' fontWeight='400'>
+                    Estimated wait time
                 </Text>
-                <Box className='queue-number'>
-                    {/* Replace 3 with function...? */}
-                    <Text fontFamily='Lush Handwritten' color= '#000' fontSize='40px'>3 Minutes</Text>
-                </Box>
+                <VStack spacing='24px' className='queue-number'>
+                    <img className='ClockIcon'src={ClockIcon} alt='Clock'/>
+                    <Text fontFamily='Helvetica' color= 'white' fontSize='30px'>3 Minutes</Text>
+                </VStack>
             </VStack>
         </Flex>
     )
