99c60fd73fa27b8c8500bfea922e1ad0cfb2080f
Armon Luckenbach <armonluck@gmail.com>
2023-09-19 15:36:52 -0400

    Import component units from Chakra UI for Drawer overlays, Store cards, and more. Also fixed file path for fonts

--- src/components/LandingHeader.jsx
+++ src/components/LandingHeader.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { SearchIcon } from '@chakra-ui/icons';
+import {
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    useDisclosure,
+    Button,
+    Stack,
+    Box,
+    Input,
+    InputGroup,
+    InputRightElement,
+    InputLeftElement,
+    Icon,
+    Flex,
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Image,
+    Heading,
+    Text,
+} from '@chakra-ui/react';
+
+import '../styles/_fonts.scss';
 
 function LandingHeader() {
   return (
-    <div>LandingHeader</div>
+    <Text fontFamily='LushHandwritten-Bold'>Landing Header</Text>
   )
 }
 
