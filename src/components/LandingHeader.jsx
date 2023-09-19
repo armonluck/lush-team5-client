963f96bd394881ad9fd66004c16a0f2cd4ec8341
Armon Luckenbach <armonluck@gmail.com>
2023-09-19 15:40:18 -0400

    Added useState variables, fx to handle changing store w/onClick(), useEffect() to get data from test server, and added basic heading using UX design styling

--- src/components/LandingHeader.jsx
+++ src/components/LandingHeader.jsx
@@ -32,9 +32,74 @@ import {
 import '../styles/_fonts.scss';
 
 function LandingHeader() {
-  return (
-    <Text fontFamily='LushHandwritten-Bold'>Landing Header</Text>
-  )
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const btnRef = React.useRef()
+
+    //API url & key
+    const local_api = "http://localhost:7272";
+
+    //State variables for video details and current/selected video
+    const [stores, setStores] = useState([]);
+    const [selectedStore, setSelectedStore] = useState({});
+
+    //handleStoreClick Function
+    const handleStoreClick = (storeId) => {
+        const selectedStoreData = stores.find((store) => store.id === storeId);
+        if (selectedStoreData) {
+            setSelectedStore(selectedStoreData);
+        } else {
+            console.error(`Store with ID ${storeId} not found.`)
+        }
+    };
+
+    //GET data for a single selected store
+    useEffect(() => {
+        if (selectedStore && selectedStore.id) {
+            axios
+                .get(`${local_api}/stores/${selectedStore.id}`)
+                .then((response) => {
+                    setSelectedStore(response.data);
+                })
+                .catch((err) => console.error(err));
+        } else {
+            axios
+                .get(`${local_api}/stores/1`) //Default store ID
+                .then((response) => {
+                    setSelectedStore(response.data);
+                })
+                .catch((err) => console.error(err));
+        }
+    }, []);
+
+    //GET all stores and set state
+    useEffect(() => {
+        axios
+            .get(`${local_api}/stores`)
+            .then((response) => {
+                setStores(response.data);
+            })
+            .catch((err) => console.error(err));
+    }, []);
+
+    return (
+        <Box p='20px' bg='#282121'>
+            <Heading
+                fontFamily='LushHandwritten-Bold' fontSize='1.5rem' color='#B9A6F0' lineHeight='normal' textShadow='-4px 5px 14px rgba(255, 255, 255, 0.15)'>
+                Welcome to Lush
+            </Heading>
+            <Text marginTop='4px' fontFamily='Helvetica' fontSize='1rem' color='#FFF' lineHeight='normal' textTransform='uppercase'>
+                Hi, Natasha
+            </Text>
+
+            <Card mt='37px' borderRadius='999px' textAlign='center' bg='#F3F2F3' boxShadow='-4px 5px 14px 0px rgba(255, 255, 255, 0.15)' h='42px' d='flex' align='center' justify='center'>
+                <CardBody>
+                    <Text fontFamily='LushHandwritten-Bold' fontWeight='700' fontSize='16px' lineHeight='normal'>
+                        Your Store: {selectedStore?.street}.
+                    </Text>
+                </CardBody>
+            </Card>
+        </Box>
+    )
 }
 
 export default LandingHeader;
