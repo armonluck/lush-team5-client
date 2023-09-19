9a8afb4a754cd9a334e440c772a72633ece68863
Vivian Yu <vivianlamyu@gmail.com>
2023-09-19 10:58:35 -0400

    Added timer function for the estimated wait time on the QueuePage

--- src/pages/QueuePage/QueuePage.jsx
+++ src/pages/QueuePage/QueuePage.jsx
@@ -1,11 +1,37 @@
 
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { Flex, Text, Box, VStack } from '@chakra-ui/react';
 import PurchasedItemBox from '../../components/PurchasedItemBox/PurchasedItemBox';
 import ClockIcon from '../../assets/icons/ph_clock.svg';
 import './QueuePage.scss'
 
 const QueuePage = () => {
+    const [remainingTime, setReaminingTime] = useState(3 * 60);
+    const [estimatedWaitTime, setEstimatedWaitTime] = useState('3:00');
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            if (remainingTime > 0) {
+                // Calculate the minutes and seconds
+                const minutes = Math.floor(remainingTime / 60);
+                const seconds = remainingTime % 60;
+                const formattedTime = `${minutes}:${String(seconds).padStart(2, '0')}`;
+
+                // Update the state variables
+                setReaminingTime(remainingTime - 1);
+                setEstimatedWaitTime(formattedTime);
+            } else {
+                // Updates the message and clears timer when countdown reaches 0
+                setEstimatedWaitTime('Your order is ready!');
+                clearInterval(timer);
+            }
+        }, 1000);
+
+        // Clears the timer when component unmounts
+        return () => clearInterval(timer);
+    }, [remainingTime]);
+
     return (
         <Flex height='100vh' background= 'var(--neutral-900, #282121)' color='#FFF' p='24px 21px 0 21px'>
             <VStack spacing={0}>
@@ -27,7 +53,7 @@ const QueuePage = () => {
                 </Text>
                 <VStack spacing='24px' className='queue-number'>
                     <img className='ClockIcon'src={ClockIcon} alt='Clock'/>
-                    <Text fontFamily='Helvetica' color= 'white' fontSize='30px'>3 minutes</Text>
+                    <Text fontFamily='Helvetica' color= 'white' fontSize='30px'>{estimatedWaitTime}</Text>
                 </VStack>
             </VStack>
         </Flex>
