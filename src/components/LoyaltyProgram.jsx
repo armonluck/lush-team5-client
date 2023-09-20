052a7a74ae977df797ec2a8931e91caaa9c861a5
Armon Luckenbach <armonluck@gmail.com>
2023-09-20 01:51:24 -0400

    Created bath bomb with svg overlay using z-index and positioning

--- src/components/LoyaltyProgram.jsx
+++ src/components/LoyaltyProgram.jsx
@@ -20,6 +20,38 @@ import '../styles/_fonts.scss';
 function LoyaltyProgram() {
     return (
         <Box p='20px' bg='#282121'>
+                        <Heading as='h2' color='#E2D7FC' fontFamily='LushHandwritten-Bold' fontSize='30px' fontWeight='700' lineHeight='normal' pb='12px'>
+                Loyalty Program
+            </Heading>
+            <Flex flexDir='column' justify='center' align='center'>
+                <Image
+                    zIndex='1'
+                    // marginTop='12px'
+                    // mx='70px'
+                    borderRadius='full'
+                    position='absolute'
+                    boxSize='250px'
+                    // boxShadow='-4px 5px 14px 0px rgba(255, 255, 255, 0.15)'
+                    src={images.OrangeBathBomb}
+                    alt='Orange circular bath bomb with swirl pattern'
+                />
+                <Image
+                    zIndex='2'
+                    position='relative'
+                    // position='absolute'
+                    // marginTop='10px'
+                    // mx='74px'
+                    top='18px'
+                    right='4px'
+                    boxSize='235px'
+                    src={images.Bubbles}
+                    alt='Purple bubbles svg overlaying a bath bomb'
+                />
+                <Text color='#FFF' fontFamily='LushHandwritten-Bold' fontSize='40px' fontWeight='700' lineHeight='normal' zIndex='3' position='relative' bottom='100px'>
+                    62%
+                </Text>
+            </Flex>
+
             <Card
                 direction='row'
                 overflow='hidden'
