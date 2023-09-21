43ae778c51ffa96cd285c73e868b6cd9e1fc5244
dilpreettoor <dilpreet.s.t@gmail.com>
2023-09-20 23:58:51 -0400

    added camera view

--- src/pages/ProductDetail/ProductDetails.jsx
+++ src/pages/ProductDetail/ProductDetails.jsx
@@ -1,4 +1,6 @@
+import React, { useRef, useEffect } from 'react';
 import ProductScan from '../../components/ProductScan/ProductScan';
+import LensVideo from '../../assets/video/lush-lens-demo.mov'
 import AboutProduct from '../../components/AboutProduct';
 import RecommendedProducts2 from '../../components/RecommendedProducts2';
 import { Box, Text } from '@chakra-ui/react';
@@ -6,8 +8,22 @@ import './ProductDetails.css';
 
 function ProductDetails() {
 
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+
+    if (video) {
+      video.addEventListener('ended', () => {
+        video.style.display = 'none';
+      });
+    }
+  }, []);
+
   return (
+    
     <Box bg='#282121' paddingBottom='20px'>
+    <video ref={videoRef} src={LensVideo} width='100%' height="auto" autoPlay />
       <ProductScan />
       <AboutProduct />
       <Text>
