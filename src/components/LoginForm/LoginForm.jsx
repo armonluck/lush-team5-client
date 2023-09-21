bb610ec320b7b2c56c805722391e92d838ed4592
Armon Luckenbach <armonluck@gmail.com>
2023-09-21 00:17:33 -0400

    Fixed minor styling for LoginPage and LoginForm

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,12 +1,12 @@
 // src/components/LoginForm.js
-import { Box, Heading, Input, Button, FormControl,Stack, Flex } from "@chakra-ui/react";
-import {Link, useNavigate} from 'react-router-dom'
+import { Box, Heading, Input, Button, FormControl, Stack, Flex } from "@chakra-ui/react";
+import { Link, useNavigate } from 'react-router-dom'
 import axios from "axios";
 import { useRef, useEffect, useState } from "react";
-import { motion, useAnimation  } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 
 
-const LoginForm = ({setLoggedIn}) => {
+const LoginForm = ({ setLoggedIn }) => {
   const navigate = useNavigate();
   // const [headingVisible, setHeadingVisible] = useState(false);
   const [formVisible, setFormVisible] = useState(false);
@@ -16,74 +16,82 @@ const LoginForm = ({setLoggedIn}) => {
   const [containerHeight, setContainerHeight] = useState("auto")
   const buttonStyle = {
     bg: "rgb(180, 157, 253)",
-    fontFamily:'Lush Handwritten Compressed',
-    ':hover':{ bg: 'white' }
+    fontFamily: 'Lush Handwritten Compressed',
+    fontSize: '24px', 
+    lineHeight: 'normal',
+    borderRadius: '10px',
+    p: '13px 46px',
+    w: '169px',
+    h: '60px',
+    ':hover': { bg: 'white' }
   }
 
   // const URL = 'http://localhost:5051/api/v1/users'
 
-    // Use useEffect to trigger the heading animation after the component mounts
-    useEffect(() => {
+  // Use useEffect to trigger the heading animation after the component mounts
+  useEffect(() => {
     //   setTimeout(() => {
     //     setHeadingVisible(true);
     //   }, 100);
-  
-      // Use another timeout to make the form visible after 1 second
-      setTimeout(() => {
-        setFormVisible(true);
-      }, 8000);
-    }, []);
-    
 
-  const handleSubmit = (e)=>{
-     e.preventDefault();
-      const {username, password} = formRef.current;
-      console.log(username.value, password.value)
-      setLoggedIn(true)
-      navigate('/home')
+    // Use another timeout to make the form visible after 1 second
+    setTimeout(() => {
+      setFormVisible(true);
+    }, 8000);
+  }, []);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { username, password } = formRef.current;
+    console.log(username.value, password.value)
+    setLoggedIn(true)
+    navigate('/home')
   }
-  
-  return (
-    <Box maxW="sm" p="0">
-        <Flex justifyContent="center" alignItems="center">
 
-                       <motion.div
-                       initial={{ opacity: 0, y: -50 }} // Initial state (hidden)
-                       animate={{ opacity: 1, y: -200 }} // Visible state
-                       transition={{ duration: 5, delay: 1 }} // Transition duration with delay
-                   >
-                <Heading as="h1" fontSize="4rem" fontFamily='Lush Handwritten Compressed' color="white">
-                  LUSH
-                </Heading>
-          </motion.div>    
-        </Flex>
-        {formVisible &&
-          <motion.div
+  return (
+    <Box maxW="full" p="0" bg='#282121'>
+      <Flex justifyContent="center" alignItems="center">
+
+        <motion.div
+          initial={{ opacity: 0, y: -50 }} // Initial state (hidden)
+          animate={{ opacity: 1, y: -200 }} // Visible state
+          transition={{ duration: 5, delay: 1 }} // Transition duration with delay
+        >
+          <Heading as="h1" fontSize="4rem" fontFamily='Lush Handwritten Compressed' color="white" marginTop='68px'>
+            LUSH
+          </Heading>
+        </motion.div>
+      </Flex>
+      {formVisible &&
+        <motion.div
           initial={{ opacity: 0 }} // Initial state (hidden)
-          animate={{ opacity: 1}}  // Visible state
+          animate={{ opacity: 1 }}  // Visible state
           transition={{ duration: 5 }} // Transition duration
           padding='0'
-      
+
         >
-        <form onSubmit={(e)=>{handleSubmit(e)}} ref={formRef}>
-          <Stack spacing="1rem" width='90vw'>
-            <FormControl id="username">
-              <Input bg= 'white' type="text" name='username' placeholder="username" fontFamily='courier' autoComplete="username"/>
-            </FormControl>
-            <FormControl id="password">
-              <Input bg= 'white' type="password" name='password' placeholder="password" autoComplete="current-password"/>
-            </FormControl>
-            <Link style={{ color: 'white' }} to='/'>Forgot Password?</Link>
-          </Stack>
-          <Flex mt='4rem' justifyContent="flex-end" alignItems="flex-end">
-              <Button type="submit" sx={buttonStyle}>
+          <form onSubmit={(e) => { handleSubmit(e) }} ref={formRef}>
+            <Stack spacing="1rem" width='90vw' mx='20px' fontFamily='Helvetica'>
+              <FormControl id="username" paddingBottom='48px'>
+                <Input bg='white' borderRadius='5px' fontSize='20px'
+                type="text" name='username' placeholder="username" autoComplete="username" />
+              </FormControl>
+              <FormControl id="password">
+                <Input bg='white' borderRadius='5px' fontSize='20px'
+                type="password" name='password' placeholder="password" autoComplete="current-password" />
+              </FormControl>
+              <Link style={{ color: 'white', fontSize: '16px', opacity: '0.7' }} to='/'>Forgot Password?</Link>
+            </Stack>
+            <Flex mt='4rem' justifyContent="flex-end" alignItems="flex-end">
+              <Button type="submit" sx={buttonStyle} marginRight='20px' marginBottom='168px'>
                 SIGN-IN
               </Button>
-          </Flex>
-        </form>
+            </Flex>
+          </form>
         </motion.div>
-        }
-      </Box>
+      }
+    </Box>
 
   );
 };
